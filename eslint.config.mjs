431d9438dfeb348a162ddc0b7e1c310998d94f5f
kvng-dev <oghuanlan@gmail.com>
2025-05-30 09:06:54 +0100

    refactor: update codebase

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  // { "@typescript-eslint/no-unused-vars": "off" },
+  { "@typescript-eslint/no-unused-vars": "off" },
 ];
 
 export default eslintConfig;
